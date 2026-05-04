51cf660202eca4183c9288234a13ea4209406661
jvvictor777 <jvjoaovictorjv777@gmail.com>
2026-05-04 12:17:39 -0300

    adiciona o menu

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print ('hello world!')
+while True:
+    print('Sistema de agendamento/consulta barbearia')
+
+    print('1. usuarios')
+    print('2. agendamentos')
+    print('3. horarios')
+    print('0. sair')
+
+    opcao = input("Escolha sua opção: ")
