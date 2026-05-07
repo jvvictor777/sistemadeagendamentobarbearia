592a80d890f0bb4df53ed589ecd80e6f5febb115
Seu Nome <jvjoaovictorjv777@gmail.com>
2026-05-07 10:08:12 -0300

    criar loop do menu

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
+import os
+
+def limpar():
+    os.system ("cls" if os.name == "nt" else 'clear')
+
+def titulo1():
+    print("______            _                     _         _____             _         ______            _                     ")
+    print("| ___ \          | |                   (_)       /  ___|           | |        | ___ \          | |                    ")
+    print("| |_/ / __ _ _ __| |__   ___  __ _ _ __ _  __ _  \ `--.  __ _ _ __ | |_ __ _  | |_/ / __ _ _ __| |__   __ _ _ __ __ _ ")
+    print("| ___ \/ _` | '__| '_ \ / _ \/ _` | '__| |/ _` |  `--. \/ _` | '_ \| __/ _` | | ___ \/ _` | '__| '_ \ / _` | '__/ _` |")
+    print("| |_/ / (_| | |  | |_) |  __/ (_| | |  | | (_| | /\__/ / (_| | | | | || (_| | | |_/ / (_| | |  | |_) | (_| | | | (_| |")
+    print("\____/ \__,_|_|  |_.__/ \___|\__,_|_|  |_|\__,_| \____/ \__,_|_| |_|\__\__,_| \____/ \__,_|_|  |_.__/ \__,_|_|  \__,_|")
+
+def menu1():
+    print("============ MENU ============")
+    print("")
+    print("[1]: Ver Horarios disponiveis.")
+    print("[2]: Marcar Horario.")
+    print("[0]: Sair.")
+    print("")
+    print("=============================")
+
+limpar()
 while True:
-    print('Sistema de agendamento/consulta barbearia')
+    titulo1()
+    menu1()
+    
+    try:
+        opcao = int(input("Digite a sua opção: "))
+    except:
+        print("Erro!!")
+        continue
 
-    print('1. usuarios')
-    print('2. agendamentos')
-    print('3. horarios')
-    print('0. sair')
+    if opcao == "1":
+        limpar()
+        print("Horarios disponiveis aqui.")
+    
+    elif opcao == '2':
+        limpar()
+        print("Agenda horario aqui.")
 
-    opcao = input("Escolha sua opção: ")
+    elif opcao == '0':
+        limpar()
+        print("Obrigado pela sua preferecia!!")
+        break
+
+    else: 
+        print("Opção invalida!!")
+        continue
